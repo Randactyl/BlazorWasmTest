4a643684e17dd70c64c7dbab9f14e027b96b3d3b
Randactyl <rafowler@randactyl.com>
2025-02-06 22:48:06 -0800

    Add project files.

---
+++ src/Randactyl.BlazorWasm/Pages/Counter.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Randactyl.BlazorWasm.Pages;
+
+public partial class Counter(ILogger<Counter> logger)
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        logger.LogDebug("Counter was {CurrentCount}", currentCount);
+        currentCount++;
+        logger.LogDebug("Counter is now {CurrentCount}", currentCount);
+    }
+}
---
+++ src/Randactyl.BlazorWasm/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Randactyl.BlazorWasm;
+using Serilog;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.BrowserConsole()
+    .CreateLogger();
+
+await builder.Build().RunAsync();
